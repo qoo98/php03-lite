16ad49bfa354b7fbecf304f45ef65c441199122f
細田空冶 <qooooo@hosodakuunoAir.AirPort>
2022-02-08 02:37:49 +0900

    Initial

---
+++ www/html/ex0/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DSN', 'mysql:host=98a491ac7a0b;dbname=logindb');
+define('DB_USER', 'root');
+define('DB_PASS', 'secret');
+?>
---
+++ www/html/ex0/top.php
@@ -0,0 +1,9 @@
+
+<h1 style="text-align:center;margin-top: 0em;margin-bottom: 1em;" class="h1_log">ログインしてください</h1>
+   <form action="login.php" method="post" class="form_log">
+     <input type="email" name="email" class="textbox un" placeholder="メールアドレス"><br>
+     <input type="password" name="password" class="textbox pass" placeholder="パスワード"><br>
+     <button type="submit" class="log_button">ログインする</button>
+   </form>
+
+<a href="top2.php">Sign up</a>
